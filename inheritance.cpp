b296f08defb6ecd1f9a4e914cc49ed9497501b2c
PhysicsNChem <rohanasharma1@gmail.com>
2026-06-28 11:47:52 -0400

    Added driver for Vehicle

--- inheritance.cpp
+++ inheritance.cpp
@@ -5,9 +5,32 @@
 #include <iostream>
 
 #include "Vehicle.h"
+#include "Car.h"
+#include "Truck.h" //add all the headers
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Vehicle Program\n" << endl;
+
+    cout << "Vehicle:" << endl; //driver for Vehicle
+
+    string manufacturer;
+    int year;
+    cout << "Enter the manufacturer: ";
+    getline(cin, manufacturer);
+
+    cout << "Enter the year built: ";
+    cin >> year;
+
+    while (cin.fail() || cin.peek() != '\n') { //failsafe
+        cin.clear();
+        cin.ignore(1000, '\n');
+        cout << "Year built: ";
+        cin >> year;
+    }
+
+    Vehicle v(manufacturer, year);
+
+    v.displayInfo();
 }
 
