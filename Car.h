5869c7ccb17a155ddc1d8dffc8331d1f0215c5a3
PhysicsNChem <rohanasharma1@gmail.com>
2026-06-28 11:17:43 -0400

    Added Car header file

---
+++ Car.h
@@ -0,0 +1,19 @@
+#pragma once
+//Rohan Sharma
+//CIS 1202 801
+//June 28 2026
+
+#include "Vehicle.h"
+
+class Car : public Vehicle { //inheritance time
+
+private:
+	int doors;
+public:
+	Car(string manufacturer, int year, int doors) //initialiser list
+		:Vehicle(manufacturer, year), doors(doors) {
+		
+	}
+	int getDoors();
+	void setDoors(int doors); //getter and setter for this
+};
