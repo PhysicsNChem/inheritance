b09fb36fba0dfcf79e48b1fad31eb675b7c42ab0
PhysicsNChem <rohanasharma1@gmail.com>
2026-06-28 10:07:17 -0400

    Defined Vehicle class

--- Vehicle.h
+++ Vehicle.h
@@ -2,3 +2,19 @@
 //Rohan Sharma
 //CIS 1202 801
 //June 28 2026
+
+#include <string>
+
+using namespace std;
+
+class Vehicle {
+private:
+	string manufacturer;
+	int yearBuilt;
+
+public:
+	Vehicle(string manufacturer, int yearBuilt) {
+		this->manufacturer = manufacturer;
+		this->yearBuilt = yearBuilt;
+	}
+};
